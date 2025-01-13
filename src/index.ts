f086de2781eb872a9afec61f4bc51c48a471582c
anubhav bisht <anubhavbisht98@gmail.com>
2025-01-13 08:05:30 +0530

    setup appollo and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import { MikroORM } from "@mikro-orm/postgresql"
 import mikroOrmConfig from "./database/orm.config";
-import { Post } from "./database/entities/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import dotenv from 'dotenv';
+import { buildSchema } from "type-graphql";
+import { TestResolver } from "./resolvers/test";
+
+dotenv.config();
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig)
     await orm.getMigrator().up()
-    // const emFork = orm.em.fork()
-    // const post = emFork.create(Post, { title: 'My first post' } as Post)
-    // await emFork.persistAndFlush(post);
-    // console.log('Post created:', post);
+    const app = express()
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [TestResolver],
+            validate: false
+        })
+    })
+    await apolloServer.start().then(() => {
+        console.log(`🚀 Graphql server is running at port http://localhost:${process.env.SERVER_PORT}/graphql`)
+    });
+    apolloServer.applyMiddleware({ app })
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`🚀 Web server is running at port ${process.env.SERVER_PORT}`)
+    })
+
 }
-main()
+
+main().catch((e) => {
+    console.error(e)
+})
