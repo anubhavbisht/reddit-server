2cd9df72a10887d19819246c0a947d6484b3ae56
anubhav bisht <anubhavbisht98@gmail.com>
2025-01-18 20:53:42 +0530

    set up graphql code genrator and using typed mutations from the same

---
+++ frontend/codegen.ts
@@ -0,0 +1,22 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:8000/graphql",
+  documents: "src/graphql/**/*.graphql",
+  generates: {
+    "src/graphql/generated/graphql.ts": {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-urql',
+      ],
+      config: {
+        withHooks: true,
+      },
+    }
+  },
+};
+
+export default config;
